af3eb136b281914a44a9ab4ec1ceeea7dbc2852e
Hans Naert <hans.naert@vives.be>
2023-10-13 19:27:18 +0200

    using ES-only module UC in CommonJs

--- myfirst.js
+++ myfirst.js
@@ -2,13 +2,18 @@ var http = require('http');
 var dt = require('./myfirstmodule.js');
 var fs = require('fs');
 
-http.createServer(function (req, res) {
-    fs.readFile('demofile1.html', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(`<html>date: ${dt.myDateTime()}`)
-        res.write(data);
-        res.end('Hello World!</html>');
-    })
-}).listen(8080);
+(async () => {
+    uc = await import('upper-case');
 
-console.log("einde programma aangepast")
+    http.createServer(function (req, res) {
+        fs.readFile('demofile1.html', function (err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(`<html>date: ${dt.myDateTime()}`)
+            res.write(uc.upperCase("Hello World!"));
+            res.write(data);
+            res.end('Hello World!</html>');
+        })
+    }).listen(8080);
+
+    console.log("einde programma aangepast")
+})();
