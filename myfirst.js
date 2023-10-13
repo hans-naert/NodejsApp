de8d3aa4087cb8687b110826d815584944568042
Hans Naert <hans.naert@vives.be>
2023-10-13 11:45:11 +0200

    add and use fs module

--- myfirst.js
+++ myfirst.js
@@ -1,9 +1,13 @@
 var http = require('http');
 var dt = require('./myfirstmodule.js');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(`date: ${dt.myDateTime()}`)
-  res.end('Hello World!');
+    fs.readFile('demofile1.html', function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(`<html>date: ${dt.myDateTime()}`)
+        res.write(data);
+        res.end('Hello World!</html>');
+    })
 }).listen(8080);
 
